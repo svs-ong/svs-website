d693ce62f408e112ef538444f3a058d0e8337ab0
Axetyy <iuliannistor06@gmail.com>
2024-09-15 20:23:06 +0300

    changed navbar filename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "@/theme";
-import NavBar from "@/components/organism/NavBar/Navbar";
+import type { Metadata } from 'next';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/theme';
+import NavBar from '@/components/organism/Navbar/Navbar';
 export const metadata: Metadata = {};
 
 export default function RootLayout({
