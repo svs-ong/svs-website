bf0a94fab9bce05afcd90fb81ac53958c668b2e5
Kimin <mihailchivu8@gmail.com>
2024-10-27 19:07:48 +0200

    yrmp

--- src/components/organism/EventCard/EventCard.tsx
+++ src/components/organism/EventCard/EventCard.tsx
@@ -25,7 +25,7 @@ export const EventCard: React.FC<EventCardProps> = ({ title, description, image,
           alignItems: "center",
         }}
       >
-        <Box sx={{ width: '500px', height: '150px', backgroundColor: theme.palette.secondary.main }}><Image src={image} alt={altImage}/></Box>
+        <Box sx={{ width: '500px', height: '150px'}}><Image src={image} alt={altImage}/></Box>
         <Stack direction="column" spacing={2}>
           <Typography variant="h5" color="primary" align="left">
             {title}
