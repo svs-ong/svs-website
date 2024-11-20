dcfc0bbd8388757f15f0b334e227737b21edeecb
Axetyy <iuliannistor06@gmail.com>
2024-11-20 16:51:27 +0200

    SectionBug

--- src/components/atomic/Section/Section.tsx
+++ src/components/atomic/Section/Section.tsx
@@ -22,27 +22,8 @@ export const Section: React.FC<SectionProps> = ({ variant, children }) => {
   };
 
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: getBackgroundColor(),
-        overflow: "hidden",
-      }}
-    >
-      <Container
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          alignItems: "center",
-          justifyContent: "center",
-          maxWidth: "100%",
-          // "@media (max-width: 600px)": {
-          //   flexDirection: "column",
-          // },
-        }}
-      >
-        {children}
-      </Container>
+    <Box sx={{ flexgrow: 1, backgroundColor: getBackgroundColor() }}>
+      <Container sx={{ alignContent: "center", justifyContent: "center" }}>{children}</Container>
     </Box>
   );
 };
