a6d34c3b29e7f74f80b624c2ec856e0229e1d52d
Axetyy <iuliannistor06@gmail.com>
2024-12-15 15:09:25 +0200

    da

--- src/app/about/components/TeamSection/team-section.tsx
+++ src/app/about/components/TeamSection/team-section.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Grid2 } from '@mui/material';
-import TeamCard from '@/app/about/components/TeamSection/team-component';
-import theme from '@/theme';
-import { Typography, Container, Box, Stack } from '@mui/material';
-import { Section } from '@/components/atomic/Section/Section';
-import { SectionVariants } from '@/components/atomic/Section/types';
+import React from "react";
+import { Grid2 } from "@mui/material";
+import TeamCard from "@/app/about/components/TeamSection/team-component";
+import theme from "@/theme";
+import { Typography, Container, Box, Stack } from "@mui/material";
+import { Section } from "@/components/atomic/Section/Section";
+import { SectionVariants } from "@/components/atomic/Section/types";
 
 interface data {
   description: string;
@@ -15,12 +15,12 @@ interface data {
 
 const mockData = [
   {
-    description: 'This is a description for card 1.',
-    imageUrl: 'https://via.placeholder.com/240',
+    description: "This is a description for card 1.",
+    imageUrl: "https://via.placeholder.com/240",
   },
   {
-    description: 'This is a description for card 2.',
-    imageUrl: 'https://via.placeholder.com/240',
+    description: "This is a description for card 2.",
+    imageUrl: "https://via.placeholder.com/240",
   },
 ];
 
@@ -28,20 +28,20 @@ const TeamSection: React.FC<data> = ({ description, imageUrl }) => {
   return (
     <Section variant={SectionVariants.primary}>
       <Stack gap="100px">
-        <Typography variant="h3" color="black" align="center" textAlign={'center'}>
+        <Typography variant="h3" color="black" align="center" textAlign={"center"}>
           Echipa Noastra
         </Typography>
         <Grid2
           container
           spacing={2}
           sx={{
-            justifyContent: '',
+            justifyContent: "",
           }}
-          direction={'row'}
+          direction={"row"}
         >
           {[1, 2, 3, 4, 5, 6].map((_, index) => {
             return (
-              <Grid2>
+              <Grid2 key={index}>
                 <TeamCard key={index} description="Andrei" imageUrl="" />
               </Grid2>
             );
